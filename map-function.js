3e6ee8a2ca55c2a913c79f4217942e82aa9d6529
Arif Bhuiya <arifulislam517@gmail.com>
2019-03-14 00:49:44 +0300

    Create a new file and added some code

---
+++ map-function.js
@@ -0,0 +1,53 @@
+//var arr = [6,8,3,4,1,5,7];
+
+
+
+// var maped =[];
+
+// for (var i = 0; i<arr.length;i++){
+//     maped.push(arr[i]+2);
+    
+// }
+// console.log('Maped arr is = '+maped);
+
+
+
+
+//  var nawMaps = arr.map(function(value){
+//     return value + 2;
+
+
+// });
+
+
+//  console.log('New Map = ' + nawMaps);
+
+
+
+
+
+
+
+
+
+
+// function myMap(arr , callback){
+
+//    var NewArr =[];
+
+//    for(var i = 0 ; i<arr.length;i++){
+//        NewArr.push(callback(arr[i]));
+
+
+//    }
+// return NewArr;
+
+// }
+
+// MyArr = myMap(arr,function(value){
+//     return value * (5-2*3);
+
+
+// });
+
+// console.log('My New Arr is = ' +MyArr);
