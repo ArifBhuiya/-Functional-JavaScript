9b926aa577c270541c34c9f06dcbed1a47942945
Arif Bhuiya <arifulislam517@gmail.com>
2019-03-14 01:00:17 +0300

    create a new file called fillter

---
+++ Filtter.js
@@ -0,0 +1,45 @@
+//var arr = [1,2,3,4,5,6,7];
+
+// var newArr = [];
+// for(var i = 0;i< arr.length;i++){
+//     if(arr[i]%2==1){
+//         newArr.push(arr[i]);
+
+//     }
+
+// }
+// console.log('New arr is = '+ newArr);
+
+
+
+//  newarr = arr.filter(function(value){
+//     return value %2 == 1;
+
+// });
+
+// console.log('New Array is = '+ newarr );
+
+
+
+
+
+
+// function myFilter(arr,callback){
+//     var newArr = [];
+
+//  for(var i = 0; i<arr.length; i++){
+//       if(callback(arr[i])){
+//           newArr.push(arr[i]);
+
+//         }
+//     }
+//     return newArr;
+
+// }
+//  myarr = myFilter(arr,function(element){
+//      return element % 2 == 1;
+
+// });
+// console.log('My Array Filter Data = S' + myarr);
+
+
