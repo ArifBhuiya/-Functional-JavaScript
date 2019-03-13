38756bbe89e5deaaf1b3a1eedada3b5bcde6467b
Arif Bhuiya <arifulislam517@gmail.com>
2019-03-14 01:13:16 +0300

    A new file called inner function

---
+++ inner-function.js
@@ -0,0 +1,40 @@
+// function outer(){
+//     var a = 10;
+//     function inner(){
+//       a;
+//         console.log('I am inner function');
+
+//     }
+    
+//     inner();
+//     console.log('I am outer function');
+// }
+
+// outer();
+
+
+// function add(a ,b){
+
+//     function sum(){
+//      return a+b;
+//     }
+
+//     function sub(){
+//         return a-b;
+
+//     }
+//     function div(){
+//         return a/b;
+
+//     }
+
+//     function mul(){
+//         return a*b;
+//     }
+
+//   return sum() + sub() + div() + mul();
+// }
+
+// var result = add(10 ,5);
+
+// console.log(result);
