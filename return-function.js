ed86605c519b3881dd787f6679caf8bee18b9bb6
Arif Bhuiya <arifulislam517@gmail.com>
2019-03-17 00:42:16 +0300

    return function

---
+++ return-function.js
@@ -0,0 +1,33 @@
+// function name(firstName,lastName,gender){
+//     if( gender==='male'){
+//        var output = 'Mr. ' +firstName + lastName;
+//        return output;
+
+//     }else if (gender === 'female'){
+//         var output = 'Mss. ' +firstName + lastName;
+//         return output;
+//     }
+// }
+
+// var fullname = name('Arif','BHuiya','male');
+
+// console.log(fullname);
+
+// function example(){
+//     return{
+//         name:'Arif Bhuiya',
+//         skill:['java','jacaScript'],
+//         print:function(){
+//             console.log(this.name,this.skill);
+            
+//         }
+//     }
+// }
+
+// var obj = example();
+
+// obj;
+
+
+
+// obj.print();
